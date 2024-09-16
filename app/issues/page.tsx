2b3dcbf7afefbb8e8120a0dad2cbe6e65d9dfd11
Adem Hussen <abudim001@gmail.com>
2024-09-15 17:46:43 -0700

    Create the issue model

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Link href="/">
+        <Button variant="classic">Edit profile</Button>{" "}
+      </Link>
+    </div>
+  );
 };
 
 export default IssuesPage;
