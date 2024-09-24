d1146e7116e0a49a76cb4583c91a490273def5a1
Adem Hussen <abudim001@gmail.com>
2024-09-24 15:51:06 -0700

    Add a delete button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,9 @@
 import prisma from "@/prisma/client";
-import { Box, Grid } from "@radix-ui/themes";
+import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "../_components/DeleteIssueButton";
 
 interface Props {
   params: { id: string };
@@ -14,13 +15,14 @@ const IssueDetailsPage = async ({ params }: Props) => {
   });
   if (!issue) notFound();
   return (
-    <Grid columns={{ initial: "1", md: "2" }} gap="5">
-      <Box>
+    <Grid columns={{ initial: "1", md: "5" }} gap="5">
+      <Box className="col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
+      <Flex direction="column" gap="4">
         <EditIssueButton issueId={issue.id} />
-      </Box>
+        <DeleteIssueButton issueId={issue.id} />
+      </Flex>
     </Grid>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Theme } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -33,7 +33,9 @@ export default function RootLayout({
           // appearance="dark"
         >
           <NavBar></NavBar>
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
           {/* <ThemePanel /> */}
         </Theme>
       </body>
