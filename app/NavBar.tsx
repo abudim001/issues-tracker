95d069ae791f35d41b0047ed0668e90af3826fab
Adem Hussen <abudim001@gmail.com>
2024-09-25 10:26:03 -0700

    Remove dublicate skeletons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -10,7 +10,7 @@ import { CiDark } from "react-icons/ci";
 
 const links = [
   { label: "Dashboard", href: "/" },
-  { label: "Issues", href: "/issues" },
+  { label: "Issues", href: "/issues/list" },
 ];
 const NavBar = () => {
   const currentPath = usePathname();
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -43,7 +43,7 @@ const IssueForm = async ({ issue }: { issue?: Issue }) => {
         await axios.post("/api/issues", data);
       }
 
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh();
     } catch (error) {
       setSubmiting(false);
