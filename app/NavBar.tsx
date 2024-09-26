8ddd9fe494189a3e47337bf425c41336417111fe
Adem Hussen <abudim001@gmail.com>
2024-09-25 18:40:26 -0700

    Add the log in and log out links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Button } from "@radix-ui/themes";
+import { Box, Button } from "@radix-ui/themes";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
@@ -15,6 +16,7 @@ const links = [
 const NavBar = () => {
   const currentPath = usePathname();
   const [darkMode, setDarkMode] = useState<boolean>(false);
+  const { status, data: session } = useSession();
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
@@ -30,21 +32,33 @@ const NavBar = () => {
       <Link href="/">
         <AiFillBug />
       </Link>
+
       <ul className="flex space-x-6 flex-grow">
         {links.map((link) => (
-          <Link
-            key={link.href}
-            className={classNames({
-              "text-violet-400": link.href === currentPath,
-              "text-zinc-500": link.href !== currentPath,
-              "hover:text-zinc-800 transition-colors": true,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                "text-violet-400": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+
+      <Box>
+        {status === "authenticated" && (
+          <Link href="/api/auth/signout">Sign out</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Sign In</Link>
+        )}
+      </Box>
+
       <Button className="space-x-20" onClick={toggleDarkMode}>
         <CiDark />
       </Button>
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
 import "./theme-config.css";
+import AuthProvider from "./auth/Provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,19 +26,21 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body>
-        <Theme
-          accentColor="violet"
-          panelBackground="solid"
-          radius="large"
-          scaling="110%"
-          // appearance="dark"
-        >
-          <NavBar></NavBar>
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-          {/* <ThemePanel /> */}
-        </Theme>
+        <AuthProvider>
+          <Theme
+            accentColor="violet"
+            panelBackground="solid"
+            radius="large"
+            scaling="110%"
+            // appearance="dark"
+          >
+            <NavBar></NavBar>
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+            {/* <ThemePanel /> */}
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
