57637ba8f93e8ed2f2507d3835fecf71d432f09a
Adem Hussen <abudim001@gmail.com>
2024-10-02 21:44:36 -0700

    Build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { Button, Flex } from "@radix-ui/themes";
+import Pagination from "./Components/Pagination";
+import { DoubleArrowLeftIcon } from "@radix-ui/react-icons";
+
 export default function Home() {
-  return <div> Hello World</div>;
+  return (
+    <Flex>
+      <Pagination itemCount={100} pageSize={10} currentPage={10} />
+    </Flex>
+  );
 }
